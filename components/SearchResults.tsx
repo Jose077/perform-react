82d1d44dd4a9c30074769a1d05f2a323148281ed
jose <sandersonqueiroz@outlook.com>
2021-11-28 18:27:01 -0300

    Adicionado virtualizacao a listagem para melhorar a performance

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react'
 import { ProductItem } from "./ProductItem"
+import { List, ListRowRenderer } from 'react-virtualized'
 
 interface SearchResultsProps {
     results: Array<{
@@ -20,23 +21,33 @@ export function SearchResults({ results, onAddToWishlist }: SearchResultsProps) 
             return total + product.price
         }, 0)
     }, [results]);
+
+    const rowRender: ListRowRenderer = ({ index, key, style }) => {
+        return (
+            <div key={key}>
+                <ProductItem
+                    product={results[index]}
+                    onAddToWishlist={onAddToWishlist}
+                /> 
+            </div>
+        )
+    }
     
 
     return (
         <div>
             <h2>{ totalPrice }</h2>
 
-            {
-                results.map((product) => {
-                    return (
-                        <ProductItem
-                            key={product.id}
-                            product={product}
-                            onAddToWishlist={onAddToWishlist}
-                        />
-                    )
-                })
-            }
+            <List
+                height={300}
+                rowHeight={30}
+                width={900}
+                overscanRowCount={5}
+                rowCount={results.length}
+                rowRenderer={rowRender}
+
+            />
+
         </div>
     )
 }
